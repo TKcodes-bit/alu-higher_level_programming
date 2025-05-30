3c971b90ed3037c2a2b02deeafa882ff8a2480cc
TKcodes-bit <t.odongo@alustudent.com>
2025-05-30 13:24:46 +0300

    Delete

--- javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// script that writes a string to a file
-const fs = require('fs');
-const myArgs = process.argv.slice(2);
-fs.writeFile(myArgs[0], myArgs[1], function (err) {
-  if (err) {
-    console.log(err);
-  }
-});
